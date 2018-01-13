23c40a5809de5f50b625f4487a35f8dfa12e49b0
jpb <jpb.06@outlook.com>
2018-01-13 11:02:18 +0100

    Util classes

---
+++ Sources/Util/DateUtil.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+namespace DateUtil
+{
+	string GetCurrentFormattedTime();
+}
---
+++ Sources/Util/FileUtil.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+#include "StringUtil.hpp"
+
+using namespace std;
+
+namespace FileUtil
+{
+	bool IsFileOpened(wstring file);
+	bool FileExists(wstring file);
+	long GetFileSize(wstring file);
+	void WriteInteger(wstring file, long integer);
+	long ReadInteger(wstring file);
+	int Copy(wstring sourceFilePath, wstring newFilePath);
+}
