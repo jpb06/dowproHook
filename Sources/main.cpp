16022cd2a325f85cca640c9005230914e49d4c0c
jpb <jpb.06@outlook.com>
2018-01-16 13:13:27 +0100

    Updated working code for communication with node through http post requests

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <winsock2.h>
 #include <Ws2tcpip.h>
+#include <sstream>
 #include <thread>
 #include "./DowPro/ReplaysWatcher.hpp"
 #include "StaticAssets.hpp"
@@ -26,6 +27,18 @@ int main()
 {
 	//init();
 
+	wstring rawGameResult = StaticAssets::SoulstormFiles.GetGameResult();
+	unique_ptr<LuaObject> parsedGameResult = StaticAssets::Lua.ParseObject(rawGameResult);
+	FileUtil::WriteFile(L"E:\\XenoCid\\Anno_2k18\\test.json", parsedGameResult->ToJson());
+
+	wstring archivePath = L"E:\\XenoCid\\Anno_2k18\\test.zip";
+	vector<wstring> filesToArchive = 
+	{ 
+		L"E:\\SteamGames\\steamapps\\common\\Dawn of War Soulstorm\\Playback\\stupidkasrkins.rec", 
+		L"E:\\XenoCid\\Anno_2k18\\test.json" 
+	};
+	StaticAssets::SoulstormFiles.ArchiveGame(archivePath, filesToArchive);
+
 	WSADATA WSAData;
 	WSAStartup(MAKEWORD(2, 0), &WSAData);
 
@@ -46,13 +59,34 @@ int main()
 	int error = connect(sock, (sockaddr*)&sin, sizeof(sin));
 	if (error != 0)
 	{
-		cout << "Error 02: conecting to server failed!\n";
+		cout << "Error 02: connecting to server failed!\n";
 		closesocket(sock);
 		return 1;
 	}
 
-	const char * header = "GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n";
-	send(sock, header, strlen(header), 0);
+	ifstream file(archivePath, ios::binary | ios::ate);
+	streamoff size = file.tellg();
+
+	stringstream out;
+	out << "POST /SendResult HTTP/1.1\r\n";
+	out << "Host: localhost:8080\r\n";
+	out << "Content-Length: " << size << "\r\n";
+	out << "\r\n";
+	string headers = out.str();
+	send(sock, headers.c_str(), headers.size(), 0);
+
+	char data[1024];
+	file.clear();
+	file.seekg(0, ios::beg);
+	while (size > 0)
+	{
+		int currRead = size >= 1024 ? 1024 : (int)size;
+		file.read(data, currRead);
+		send(sock, data, currRead, 0);
+
+		size -= currRead;
+	}
+	file.close();
 
 	char answ[1024];
 	while (recv(sock, answ, 1024, 0) != 0)
@@ -60,6 +94,5 @@ int main()
 
 	WSACleanup();
 	
-	cin.get();
 	return 0;
 }
