5cbfe365bfa690dfde52e0327c0b913c70dd95da
jpb <jpb.06@outlook.com>
2018-01-16 07:49:14 +0100

    Some research to make http requests to node

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <winsock2.h>
+#include <Ws2tcpip.h>
 #include <thread>
 #include "./DowPro/ReplaysWatcher.hpp"
 #include "StaticAssets.hpp"
 #include "Util\FileUtil.hpp"
+
+
 
 using namespace std;
 
@@ -20,7 +24,41 @@ void init()
 
 int main()
 {
-	init();
+	//init();
+
+	WSADATA WSAData;
+	WSAStartup(MAKEWORD(2, 0), &WSAData);
+
+	SOCKET sock;
+	
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		cout << "Error 01: creating socket failed!\n";
+		closesocket(sock);
+		return 1;
+	}
+
+	SOCKADDR_IN sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(8080);
+	inet_pton(AF_INET, "127.0.0.1", &sin.sin_addr);
+
+	int error = connect(sock, (sockaddr*)&sin, sizeof(sin));
+	if (error != 0)
+	{
+		cout << "Error 02: conecting to server failed!\n";
+		closesocket(sock);
+		return 1;
+	}
+
+	const char * header = "GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n";
+	send(sock, header, strlen(header), 0);
+
+	char answ[1024];
+	while (recv(sock, answ, 1024, 0) != 0)
+		cout << answ << endl;
+
+	WSACleanup();
 	
 	cin.get();
 	return 0;
