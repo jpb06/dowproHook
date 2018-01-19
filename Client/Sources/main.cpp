b2c8514408f6355c1fd6b982d251a84969e066c3
jpb <jpb.06@outlook.com>
2018-01-19 07:25:35 +0100

    Using crypto functions

--- Client/Sources/main.cpp
+++ Client/Sources/main.cpp
@@ -1,10 +1,11 @@
-#include "Network/Socket.hpp"
 #include <iostream>
 #include "./DowPro/ReplaysWatcher.hpp"
 #include "StaticAssets.hpp"
 #include "Util\FileUtil.hpp"
 
 
+#include ".\Crypto\picosha2.hpp"
+#include ".\Crypto\Base64.hpp"
 
 using namespace std;
 
@@ -20,12 +21,25 @@ void init()
 	replaysWatcher.Start();
 }
 
+void launchSoulstorm()
+{
+	// lauching soulstorm
+	system("start steam://rungameid/9450");
+}
+
 int main()
 {
 	//init();
 
-	// lauching soulstorm
-	system("start steam://rungameid/9450");
-	
+	ifstream ifs("E:\\XenoCid\\Anno_2k18\\dowproHook\\test.zip", ios_base::in | ios_base::binary);
+	cout << picosha2::hash256_hex_string(string(istreambuf_iterator<char>(ifs), istreambuf_iterator<char>())) << endl;
+
+	//--------------------------------------------------------------
+	/*string src_str = "The quick brown fox jumps over the lazy dog";
+	string hash_hex_str;
+	picosha2::hash256_hex_string(src_str, hash_hex_str);
+	cout << hash_hex_str << endl;*/
+
 	return 0;
 }
+
