ca9671330c9f6c8f21c505d5d4c33a527de821c7
jpb <jpb.06@outlook.com>
2018-01-26 22:42:14 +0100

    Server : Api types

---
+++ ServerTestSendFile/src/Api/Types/GamePlayer.ts
@@ -0,0 +1,19 @@
+﻿export interface GamePlayer {
+    PRace: string;
+    PHuman: number;
+    PFnlState: number;
+    PTeam: number;
+    PName: string;
+    PTtlSc: number;
+}
+
+export function isGamePlayer(obj: any): obj is GamePlayer {
+    return (
+        typeof obj.PRace === "string" &&
+        typeof obj.PHuman === "number" &&
+        typeof obj.PFnlState === "number" &&
+        typeof obj.PTeam === "number" &&
+        typeof obj.PName === "string" &&
+        typeof obj.PTtlSc === "number"
+    );
+}
