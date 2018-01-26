75de78f4b19575fcf270f5ab1a97bf34db37beab
jpb <jpb.06@outlook.com>
2018-01-26 22:42:27 +0100

    Server : Api functions

---
+++ ServerTestSendFile/src/Api/SendReplays.ts
@@ -0,0 +1,39 @@
+﻿import * as util from 'util';
+import * as fs from 'fs';
+import * as yauzl from 'yauzl';
+
+export async function Handle(data: Buffer): Promise<boolean> {
+    const fs_writeFile = util.promisify(fs.writeFile);
+    await fs_writeFile('test.zip', data);
+
+    yauzl.open('test.zip', { lazyEntries: true }, (err, zipfile) => {
+
+        if (err) throw err;
+
+        zipfile.readEntry();
+
+        zipfile.on('entry', (entry) => {
+            if (/\/$/.test(entry.fileName)) {
+                // Directory file names end with '/'.
+                zipfile.readEntry();
+            } else {
+                // file entry
+                zipfile.openReadStream(entry, function (err, readStream) {
+                    if (err) throw err;
+                    readStream.on("end", function () {
+                        zipfile.readEntry();
+                    });
+                    var myFile = fs.createWriteStream(entry.fileName);
+                    readStream.pipe(myFile);
+                });
+            }
+
+            zipfile.readEntry();
+        });
+        zipfile.on('end', () => {
+            console.log('end');
+        });
+    });
+
+    return true;
+}
---
+++ ServerTestSendFile/src/Api/SendResult.ts
@@ -0,0 +1,18 @@
+﻿import { GameResultRequest, isGameResultRequest } from './Types/GameResultRequest';
+
+export async function Handle(data: Buffer): Promise<boolean> {
+    let raw = data.toString('utf8');
+
+    let json: string;
+    try {
+        json = JSON.parse(raw);
+    } catch(e) {
+        console.log(e);
+        return false;
+    }
+
+    if (!isGameResultRequest(json)) return false;
+    let gameResultRequest = json as GameResultRequest;
+
+    return true;
+}
